48e4476c14b17e39b8eefd371e5f3f055cc6ebff
Laura <lg3295@gmail.com>
2016-04-04 20:07:01 -1000

    returns html file and header with correct request, otherwise gives status 404

---
+++ server.js
@@ -0,0 +1,51 @@
+var net = require('net');
+var server = net.createServer(function(request){
+
+  var html404 = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Element not found!</title><link rel=\"stylesheet\" href=\"/css/styles.css\"></head><body><h1>404</h1><h2>Element not found!</h2><p><a href=\"/\">back</a></p></body></html>";
+
+  var helium = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>The Elements - Helium</title><link rel=\"stylesheet\" href=\"/css/styles.css\"></head><body><h1>Helium</h1><h2>H</h2><h3>Atomic number 2</h3><p>Helium is a chemical element with symbol He and atomic number 2. It is a colorless, odorless, tasteless, non-toxic, inert, monatomic gas that heads the noble gas group in the periodic table. Its boiling and melting points are the lowest among all the elements and it exists only as a gas except in extremely cold conditions.</p><p><a href=\"/\">back</a></p></body></html>";
+
+  var hydrogen = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>The Elements - Hydrogen</title><link rel=\"stylesheet\" href=\"/css/styles.css\"></head><body><h1>Hydrogen</h1><h2>H</h2><h3>Atomic number 1</h3><p>Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the universe, constituting roughly 75% of all baryonic mass. Non-remnant stars are mainly composed of hydrogen in its plasma state. The most common isotope of hydrogen, termed protium (name rarely used, symbol 1H), has a single proton and zero neutrons.</p><p><a href=\"/\">back</a></p></body></html>";
+
+    var index = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>The Elements</title><link rel=\"stylesheet\" href=\"/css/styles.css\"></head><body><h1>The Elements</h1><h2>These are all the known elements.</h2><h3>These are 2</h3><ol><li><a href=\"/hydrogen.html\">Hydrogen</a></li><li><a href=\"/helium.html\">Helium</a></li></ol></body></html>";
+
+
+  request.on('data', function(data){
+    var myData = data.toString();
+    var date = new Date();
+    date = date.toUTCString();
+  if (myData.indexOf("GET /404.html") !== -1){
+
+    request.write(
+      "status: 200, date: " + date +", server: LG Servers" + html404
+     );
+  } else if (myData.indexOf("GET /helium.html") !== -1){
+    request.write(
+      "status: 200, date: " + date +", server: LG Servers" + helium
+     );
+  } else if (myData.indexOf("GET /hydrogen.html") !== -1){
+    request.write(
+      "status: 200, date: " + date +", server: LG Servers" + hydrogen
+     );
+  } else if ((myData.indexOf("GET /index.html") !== -1) || (myData.indexOf("GET / ") !== -1)){
+    request.write(
+      "status: 200, date: " + date +", server: LG Servers" + index
+     );
+  } else {
+    request.write("status: 404 Not Found, date: " + date + ", server: LG Servers");
+  }
+
+
+    request.end();
+  });
+
+  request.on('end', function(){
+    console.log('Socket connection ended');
+  });
+
+});
+
+server.listen({port: 8080}, function(){
+  var address = server.address();
+  console.log('opened server on %d', address.port);
+});
