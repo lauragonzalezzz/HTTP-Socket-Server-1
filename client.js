4d5595e3d97c2d7afa02b9b99cb411a27a5c5f81
Laura <lg3295@gmail.com>
2016-04-05 16:47:53 -1000

    can access the web, but devleague is hard coded into the host parameter

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var net = require('net');
 var stream = require('stream');
 
-var client = net.createConnection({port: 8080}, function(){
+var client = net.createConnection({host: 'www.devleague.com', port: 80}, function(){
   var path = process.argv[2];
 
   var date = new Date();
@@ -15,20 +15,19 @@ var client = net.createConnection({port: 8080}, function(){
     client.end();
   } else {
       client.write(
-        'GET ' + path + ' HTTP/1.1 \n' +
+        'GET / HTTP/1.1 \n' +
         'Date: ' + date + '\n' +
-        'Host: localhost' +
-        'User-Agent: MEEEEeeeeEeEeEEee!')
+        'Host: www.devleague.com \n' +
+        'User-Agent: MEEEEeeeeEeEeEEee! \r\n\r\n')
   };
 });
 
 
 client.on('data', function(data){
+  console.log(data.toString());
 
   var responseHeadersArr = [];
-
   var responseHeader = data.toString().split('\n\n')[0].split('\n');
-
   responseHeadersArr.push(responseHeader);
 
   client.end();
