b8871f222c975bb619f363b393aa1d0c96064cac
Laura <lg3295@gmail.com>
2016-04-05 14:42:48 -1000

    client requests data from server

--- client.js
+++ client.js
@@ -2,6 +2,12 @@ var net = require('net');
 
 var client = net.createConnection({port: 8080}, function(){
   console.log('connected to server!');
+  client.write('GET /index.html HTTP/1.1');
+});
+
+client.on('data', function(data){
+  console.log(data.toString());
+  client.end();
 });
 
 client.on('end', function(){
