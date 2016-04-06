69530035b2c94c57b4db1d9e09f3042c79763275
Laura <lg3295@gmail.com>
2016-04-05 16:15:01 -1000

    uses process.argv to take input from the user and place it into the request for the server

--- client.js
+++ client.js
@@ -1,15 +1,26 @@
 var net = require('net');
+var stream = require('stream');
 
 var client = net.createConnection({port: 8080}, function(){
-  console.log('connected to server!');
-  client.write('GET /index.html HTTP/1.1');
+  var path = process.argv[2];
+
+  var date = new Date();
+  date = date.toUTCString();
+
+  client.write(
+    'GET ' + path + ' HTTP/1.1 \n' +
+    'Date: ' + date + '\n' +
+    'Host: localhost' +
+    'User-Agent: MEEEEeeeeEeEeEEee!');
 });
 
 client.on('data', function(data){
   console.log(data.toString());
+
   client.end();
 });
 
 client.on('end', function(){
   console.log('disconnected from server');
 });
+
